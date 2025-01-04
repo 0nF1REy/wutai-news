47371b19686420bbb8ece450875f85e9f795729b
Alan Ryan <alanryan619@gmail.com>
2025-01-03 23:49:49 -0300

    refactor: Added software architecture and route soma

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-const express = require('express')
-const app = express()
+const express = require("express")
+const userRoute = require("./src/routes/user.route")
+const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Olá Mundo!!!')
-})
+app.use("/soma", userRoute);
+
+// Rota
+    // Method HTTP - CRUD (CREATE, READ, UPDATE, DELETE)
+        // GET - Pega uma info
+        // POST - Cria uma info
+        // PUT - Altera toda a info
+        // PATCH - Altera parte da info
+        // DELETE - Apaga uma info
+
+    // Name - Um identificador da rota
+
+    // Function (Callback) - responsável por executar algum comando
+ 
+    
+// app.get('/soma', (req, res) => {
+//   const soma = (100 + 1);
+//   res.send({soma: soma})
+// })
 
 app.listen(3000)
