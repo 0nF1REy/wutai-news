e92505192a0ffe892c6dd79c70c44df9168f1831
Alan Ryan <alanryan619@gmail.com>
2025-01-06 18:27:07 -0300

    refactor: config route create user and controller

--- index.js
+++ index.js
@@ -1,25 +1,12 @@
 const express = require("express")
-const userRoute = require("./src/routes/user.route")
+
 const app = express();
 
-app.use("/soma", userRoute);
+const userRoute = require("./src/routes/user.route")
 
-// Rota
-    // Method HTTP - CRUD (CREATE, READ, UPDATE, DELETE)
-        // GET - Pega uma info
-        // POST - Cria uma info
-        // PUT - Altera toda a info
-        // PATCH - Altera parte da info
-        // DELETE - Apaga uma info
+const port = 3000;
 
-    // Name - Um identificador da rota
+app.use(express.json());
+app.use("/user", userRoute);
 
-    // Function (Callback) - responsável por executar algum comando
- 
-    
-// app.get('/soma', (req, res) => {
-//   const soma = (100 + 1);
-//   res.send({soma: soma})
-// })
-
-app.listen(3000)
+app.listen(3000, () => console.log(`Servidor rodando na porta ${port}`));
