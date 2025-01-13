fb59f9555dffb5c89ea11c20ff1a7a7cb72308ba
Alan Ryan <alanryan619@gmail.com>
2025-01-13 18:00:09 -0300

    feat: Installed the cors package (Cross-Origin Resource Sharing) and configured it.

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-import express from 'express';
-import connectDatabase from './src/database/db.js';
+import express from "express";
+import connectDatabase from "./src/database/db.js";
 import dotenv from "dotenv";
+import cors from "cors";
 
-import userRoute from './src/routes/user.route.js';
-import authRoute from './src/routes/auth.route.js';
-import newsRoute from './src/routes/news.route.js';
-import swaggerRoute from './src/routes/swagger.route.js';
+import userRoute from "./src/routes/user.route.js";
+import authRoute from "./src/routes/auth.route.js";
+import newsRoute from "./src/routes/news.route.js";
+import swaggerRoute from "./src/routes/swagger.route.js";
 
 dotenv.config();
 
@@ -13,7 +14,11 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 connectDatabase();
+
+app.use(cors());
+
 app.use(express.json());
+
 app.use("/user", userRoute);
 app.use("/auth", authRoute);
 app.use("/news", newsRoute);
